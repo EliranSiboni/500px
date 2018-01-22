216738ad08a564f21392c39432bb8205b804f0d9
EliranSiboni <eliran.siboni@gmail.com>
2018-01-22 14:23:00 +0200

    debug

--- javaScriptPhotosNav.js
+++ javaScriptPhotosNav.js
@@ -1,48 +1,41 @@
-
-
 //sticky nav bar
 
 
 
-window.onscroll = function() {myFunction()};
-
-var navbar = document.getElementById("navbar");
-var sticky = navbar.offsetTop;
-
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.remove("display");
-    navbar.classList.add("sticky");
-
-  } else {
-    navbar.classList.remove("sticky");
-    navbar.classList.add("display");
+window.addEventListener("scroll",function(){
+  var target = document.getElementById('navbar');
+  if(window.pageYOffset > 600){
+   target.style.display = "block"; 
   }
-}
+  else if(window.pageYOffset < 600){
+    target.style.display = "none";
+  }
+},false);
+
 
 //Grid photos buttons tabs.
 
 
 var btns = document.querySelectorAll('.navGridSection1text');
 
-var texts = document.querySelectorAll(".displayNone")
+var texts = document.querySelectorAll(".displayNone");
 
 document.querySelector('#text_1').classList.add('active');
 
-    	for (var i = 0; i < 5 ; i++) {
-    		btns[i].addEventListener('click', function  (event) {
-    		 console.log(event.target);
-    		 var textNumber = event.target.value;
+for (var i = 0; i < 5; i++) {
+    btns[i].addEventListener('click', function(event) {
+        console.log(event.target);
+        var textNumber = event.target.value;
 
-    		 hideAllTexts();
-    		 document.querySelector('#text_' + textNumber ).classList.add('active')
+        hideAllTexts();
+        document.querySelector('#text_' + textNumber).classList.add('active');
 
 
-    		})
-    	}
+    });
+}
 
-    	function hideAllTexts (argument) {
-    	for (var i = 0; i < texts.length; i++) {
-    		texts[i].classList.remove('active');
-    	}
-    	}
+function hideAllTexts(argument) {
+    for (var i = 0; i < texts.length; i++) {
+        texts[i].classList.remove('active');
+    }
+}
